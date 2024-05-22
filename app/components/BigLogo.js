b2aab138b37e05ce31b707256f4d9e4d34879a48
Marisol <marisol.azofeifa@totemtech.net>
2024-05-22 12:16:34 -0600

    height

--- app/components/BigLogo.js
+++ app/components/BigLogo.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const BigLogo = () => {
   return (
     <div className='h-screen w-full flex justify-center items-center absolute top-0 pointer-events-none'>
-        <motion.div layoutId="logo" className='w-3/4 md:w-1/2 h-40 relative'>
+        <motion.div layoutId="logo" className='w-3/4 md:w-1/2 h-16 md:h-40 relative'>
         <Image
             className="object-contain"
             src={`/jps-logo.svg`}
